3f4235f4f106f4f7fc43a109f404768c06ac2a0f
Steve Cordle <stevec@allegranetwork.com>
2015-01-23 16:52:42 +0100

    Initial Commit

---
+++ gravity-forms-bulk-download.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Gravity Forms Bulk Download
+* Plugin URI: http://stevecordl.net
+* Description: Bulk download files for file upload forms using Gravity Forms
+* Version: 0.9
+* Author: Steve Cordle
+* Author URI: http://stevecordle.net/
+**/
