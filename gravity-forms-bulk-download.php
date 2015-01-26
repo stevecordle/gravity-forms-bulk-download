e7d03c1951595320a88bbf65ccf47c8e87864879
Steve Cordle <stevec@allegranetwork.com>
2015-01-26 10:53:11 -0500

    Switched after submission hook to gform_entry_created so the entry can be manipulated before the notification is sent out, added style hook, cleaned up file a bit

--- gravity-forms-bulk-download.php
+++ gravity-forms-bulk-download.php
@@ -13,7 +13,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Alchemy\Zippy\Zippy;
 
 if (class_exists("GFForms") && !class_exists("GFBulkDownload")) {
-    GFForms::include_feed_addon_framework();
 
     class GFBulkDownload {
 
@@ -21,11 +20,21 @@ if (class_exists("GFForms") && !class_exists("GFBulkDownload")) {
         protected $version = '1.0';
 
         public function __construct() {
-            add_action("gform_after_submission", array($this, 'afterSubmission'), 10, 2);
+            add_action("admin_enqueue_scripts", array($this, 'addStyles'));
+            add_action("gform_entry_created", array($this, 'afterSubmission'), 1, 2);
             add_action("gform_pre_submission_filter", array($this, 'preSubmissionFilter'), 10, 1);
             add_action("gform_entries_column_filter", array($this, 'change_column_data'), 10, 5);
             add_filter('gform_custom_merge_tags', array($this, 'custom_merge_tags'), 10, 4);
             add_filter('gform_replace_merge_tags', array($this, 'replace_download_link'), 10, 7);
+        }
+        
+        public function addStyles(){
+            wp_register_style('gform_bulk_download_css', plugin_dir_url(__FILE__).'assets/css/style.css');
+            wp_enqueue_style('gform_bulk_download_css');
+        }
+        
+        public function addScripts(){
+           
         }
 
         public function preSubmissionFilter($form) {
@@ -39,7 +48,7 @@ if (class_exists("GFForms") && !class_exists("GFBulkDownload")) {
             //decode json and remove slashes to get list of files
             $files_for_zip = json_decode(stripslashes($_POST['gform_uploaded_files']), true);
 
-            //rest the array pointer
+            //reset the array pointer
             reset($files_for_zip);
 
             // get the key of the first element (This is due to the format of the array  )          
@@ -57,8 +66,9 @@ if (class_exists("GFForms") && !class_exists("GFBulkDownload")) {
             $zip_name = 'files_' . md5(rand(0123456, 7890123)) . '.zip';
             //Create and save the zip file
             $zip->create(trailingslashit($form_upload_dir) . $zip_name, $files, true);
-            //Set the zip key for the $form object to our zip url so we can pass it to the entry
+            //Set the zip key for the $form object to the url for the zip file we created, minus the domainname so we can pass it to the entry
             $form['zip'] = trailingslashit($form_upload_dir_formatted) . $zip_name;
+            
             return $form;
         }
 
@@ -74,7 +84,6 @@ if (class_exists("GFForms") && !class_exists("GFBulkDownload")) {
             $zip_path = gform_get_meta($lead['id'], 'zip_file');
             $site_url = get_bloginfo('url');
             $full_url = trailingslashit($site_url) . $zip_path;
-
             foreach ($form['fields'] as $field) {
                 if ($field['type'] == 'fileupload' && $field['multipleFiles'] == "1") {
                     $upload_field_id = $field['id'];
@@ -82,33 +91,29 @@ if (class_exists("GFForms") && !class_exists("GFBulkDownload")) {
             }
             if ($upload_field_id == $field_id) {
                 if (!empty($zip_path)) {
-                    return $value . " <a style='margin-left:20px;' href='$full_url'> Download All <i class='fa fa-download'> </i></a>";
+                    return $value . " <a class='gfbd-btn' href='$full_url'> Download All <i class='fa fa-download'> </i></a>";
                 }
             }
-
             return $value;
         }
 
         function custom_merge_tags($merge_tags, $form_id, $fields, $element_id) {
-
             $merge_tags[] = array('label' => 'Download All Files (Zip)', 'tag' => '{download_all_files_zip}');
             return $merge_tags;
         }
 
         function replace_download_link($text, $form, $entry, $url_encode, $esc_html, $nl2br, $format) {
             $custom_merge_tag = '{download_all_files_zip}';
-
-            if(strpos($text, $custom_merge_tag) === false){
+            if (strpos($text, $custom_merge_tag) === false) {
                 return $text;
-            }else{
+            } else {
                 $zip_path = gform_get_meta($entry['id'], 'zip_file');
-                $full_url = trailingslashit(get_bloginfo('url')).$zip_path;
-                $text = str_replace($custom_merge_tag, $full_url, $text);
-                return $text;
+                $full_url = trailingslashit(get_bloginfo('url')) . $zip_path;
+                return str_replace($custom_merge_tag, $full_url, $text);
             }
         }
+        
+    }
 
-}
-
-new GFBulkDownload();
+    new GFBulkDownload();
 }
